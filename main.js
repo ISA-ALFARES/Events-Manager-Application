3093c51f37ae5f066a9fad58b349a151307fd45a
isa <isa430752@gmail.com>
2024-06-02 16:09:43 +0300

    Modify and configure date settings

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     flatpickr(".event-date", {
       dateFormat: "Y-m-d",  // Set date format
-      minDate: "2024-06-06",  // Set minimum date
-      maxDate: "",  // Set maximum date
-      theme: "dark",  // Set theme
+      minDate: new Date().toISOString().split("T")[0],  // Set minimum date
+      maxDate: "" // Set maximum date
       // Additional options and customizations
-      defaultDate: "Select Date"   // Set default date
 
     });
   });
